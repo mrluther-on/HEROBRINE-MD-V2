4085b7dd5a459a9cb771f73e01ab621daf80f7ac
MR LUTHER <lutherofficial009@gmail.com>
2025-02-07 17:03:20 +0530

    Update and rename QUEEN-HENTAI-ON.js to HEROBRINE-ON.js

--- plugins/HEROBRINE-ON.js
+++ plugins/HEROBRINE-ON.js
@@ -4,31 +4,31 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 	
 const sections = [
    {
-	title: `💝 Queen Hentai 💝`,
+	title: ` HEROBRINE `,
 	rows: [
-	{title: "🇱🇰 | Welcome", rowId: `${usedPrefix + command} welcome`},
-	{title: "🇱🇰 | Public", rowId: `${usedPrefix + command} public`},
-	{title: "🇱🇰 | Nsfw", rowId: `${usedPrefix + command} nsfw`},
-	{title: "🇱🇰 | OnlyEnglish", rowId: `${usedPrefix + command} onlyenglish`},
-	{title: "🇱🇰 | Antilink", rowId: `${usedPrefix + command} antilink`},
-    {title: "🇱🇰 | Antidelete", rowId: `${usedPrefix + command} antidelete`},
-    {title: "🇱🇰 | Autosticker", rowId: `${usedPrefix + command} autosticker`},
-	{title: "🇱🇰 | Autolevelup", rowId: `${usedPrefix + command} autolevelup`},
-	{title: "🇱🇰 | ChatBot", rowId: `${usedPrefix + command} chatbot`},
-	{title: "🇱🇰 | Detect", rowId: `${usedPrefix + command} detect`},
-	{title: "🇱🇰 | Document", rowId: `${usedPrefix + command} document`},
-	{title: "🇱🇰 | Restrict", rowId: `${usedPrefix + command} restrict`},
-	{title: "🇱🇰 | OnlyPvivate", rowId: `${usedPrefix + command} onlydm`},
-	{title: "🇱🇰 | OnlyGroup", rowId: `${usedPrefix + command} onlygp`}
+	{title: " | Welcome", rowId: `${usedPrefix + command} welcome`},
+	{title: " | Public", rowId: `${usedPrefix + command} public`},
+	{title: " | Nsfw", rowId: `${usedPrefix + command} nsfw`},
+	{title: " | OnlyEnglish", rowId: `${usedPrefix + command} onlyenglish`},
+	{title: " | Antilink", rowId: `${usedPrefix + command} antilink`},
+    {title: " | Antidelete", rowId: `${usedPrefix + command} antidelete`},
+    {title: " | Autosticker", rowId: `${usedPrefix + command} autosticker`},
+	{title: " | Autolevelup", rowId: `${usedPrefix + command} autolevelup`},
+	{title: " | ChatBot", rowId: `${usedPrefix + command} chatbot`},
+	{title: " | Detect", rowId: `${usedPrefix + command} detect`},
+	{title: " | Document", rowId: `${usedPrefix + command} document`},
+	{title: " | Restrict", rowId: `${usedPrefix + command} restrict`},
+	{title: " | OnlyPvivate", rowId: `${usedPrefix + command} onlydm`},
+	{title: " | OnlyGroup", rowId: `${usedPrefix + command} onlygp`}
 	]
     },
 ]
 
 const listMessage = {
-  text: '\nThis is the list of 💝 Queen Hentai 💝 Now you can turn on and off it',
+  text: '\nThis is the list of HEROBRINE Now you can turn on and off it',
   footer: igfg,
-  title: `💝 Queen Hentai 💝`,
-  buttonText: "💝 Click here 💝",
+  title: `HEROBRINE`,
+  buttonText: " Click here 💝",
   sections
 }
 
