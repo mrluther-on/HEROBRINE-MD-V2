0c9047f3d65f05b632cdab7bae436c401a27e068
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 13:02:31 +0530

    Update and rename QUEEN-HENTAI-donate.js to HEROBRINE-donate.js

--- plugins/HEROBRINE-donate.js
+++ plugins/HEROBRINE-donate.js
@@ -2,8 +2,8 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
-🇱🇰 *DONATE* 🇱🇰
-If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
+🇮🇳 *DONATE* 🇮🇳
+If you get more info of ⚡ＨＥＲＯＢＲＩＮＥ 🧩 Please Contact 919614477116 💰`
 let img = 'https://telegra.ph/file/7d84f663bc6e7fdac5c66.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
