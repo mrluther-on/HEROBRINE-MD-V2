6ec87838ab157cdb5d6eadb8d99b0ae820d7eb31
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 13:25:33 +0530

    Update QUEEN-HENTAI-tourl.js

--- plugins/QUEEN-HENTAI-tourl.js
+++ plugins/QUEEN-HENTAI-tourl.js
@@ -4,11 +4,11 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '🔊 Respond an image/video to 💝 Queen Hentai 💝'
+  if (!mime) throw '🔊 Respond an image/video to ⚡ＨＥＲＯＢＲＩＮＥ 🧩'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`💝 Queen Hentai 💝
+  m.reply(`⚡ＨＥＲＯＢＲＩＮＥ 🧩
       link convertor
 
 
