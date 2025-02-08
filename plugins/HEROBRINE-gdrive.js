929891228fe603a6653f9f1e99dc7e85c0cd8cf0
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 13:05:51 +0530

    Update and rename QUEEN-HENTAI-gdrive.js to HEROBRINE-gdrive.js

--- plugins/HEROBRINE-gdrive.js
+++ plugins/HEROBRINE-gdrive.js
@@ -2,21 +2,21 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `💝 Queen Hentai 💝, Enter a Google Drive link`
+	if (!args[0]) throw `⚡ＨＥＲＯＢＲＩＮＥ 🧩, Enter a Google Drive link`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-*💝 Queen Hentai 💝 Google Drive DOWNLOADER*
+*⚡ＨＥＲＯＢＲＩＮＥ 🧩 Google Drive DOWNLOADER*
 
-💝 *Number:* ${res.fileName}
-💝 *Size:* ${res.fileSize}
-💝 *type:* ${res.mimetype}`)
+🧃 *Number:* ${res.fileName}
+🧃 *Size:* ${res.fileSize}
+🧃 *type:* ${res.mimetype}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('💝 Error: Check the link or try another link 💝') 
+	m.reply('🧃 Error: Check the link or try another link 🧃') 
   }
 }
 handler.help = ['gdrive']
