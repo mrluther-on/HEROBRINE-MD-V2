0c42c3c358397e4aaa01c308395e912f58514d4b
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 14:04:18 +0530

    Update and rename main-Hentai.js to main-HEROBRINE.js

---
+++ plugins/main-HEROBRINE.js
@@ -0,0 +1,21 @@
+let handler = async m => m.reply(`
+
+   *⚡ＨＥＲＯＢＲＩＮＥ 🧩* WA Group
+🔊 Join Public Bot Group And Support
+https://chat.whatsapp.com/FyzcZHkAqBNEuRY4XAEOSb
+─────────────
+🔊 *Github*
+https://github.com/mrluther-on/HEROBRINE-MD-V2
+ 
+🔊 *Contact*
+wa.me:919614477116
+
+🔊 *Instagram*
+https://instagram.com/lexx_lutheer_
+
+`.trim())
+handler.help = ['gphero']
+handler.tags = ['main']
+handler.command = ['groups', 'supporthero', 'herobrinegp', 'groupbrine', 'group'] 
+
+export default handler
