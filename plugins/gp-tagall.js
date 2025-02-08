fd8db2578a9579e12f9c8b6b509c538963afe43b
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 13:57:48 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌───⊷ *Mention By  —(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—*\n` + users.map(v => '💝 @' + v.replace(/@.+/, '')).join`\n` + '\n└──*—(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—*──', null, {
+    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌───⊷ *Mention By  —(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—*\n` + users.map(v => '🇮🇳  @' + v.replace(/@.+/, '')).join`\n` + '\n└──*—(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—*──', null, {
         mentions: users
     })
 }
