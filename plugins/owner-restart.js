56a592a15ff3978acea9697cbdc0a1fbb5095748
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 14:07:21 +0530

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🇱🇰 Restarting *💝 Queen Hentai 💝* WA BOT...\n Plzz waiting..ohoma poddk idm besikayo🤣')
+    await m.reply(' Restarting *⚡ＨＥＲＯＢＲＩＮＥ 🧩* WA BOT...\n Plzz waiting..ohoma poddk idm besikayo🤣')
     process.send('reset')
   } else throw 'eh'
 }
