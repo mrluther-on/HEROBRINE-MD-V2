21920b33220bf65e280afc7e69e3c6686317cc30
MR LUTHER <lutherofficial009@gmail.com>
2025-02-08 13:17:33 +0530

    Update QUEEN-HENTAI-setbye.js

--- plugins/QUEEN-HENTAI-setbye.js
+++ plugins/QUEEN-HENTAI-setbye.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
     m.reply('😤 Sirwatama, Do you want to change bye message')
-  } else throw `🇱🇰 ehenam bza enter the message\n@user (mención)`
+  } else throw `⚡ＨＥＲＯＢＲＩＮＥ a enter the message\n@user (mención)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
